38e2b320eae58f30ad7b67686af56c8244f70e0f
Krilll <Krilll@users.noreply.github.com>
2019-05-20 03:08:29 +0300

    add homework 2

---
+++ frontend/views/site/hello.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Hello!</h1>
+
+        <h2>It is a frontend-page</h2>
+
+    </div>
+</div>
