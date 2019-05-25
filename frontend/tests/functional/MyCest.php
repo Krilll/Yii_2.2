266a0de67caf6f9cc56f6b792fa4dbbe36c90bbb
Krilll <Krilll@users.noreply.github.com>
2019-05-25 14:36:33 +0300

    add 2.2

--- frontend/tests/functional/MyCest.php
+++ frontend/tests/functional/MyCest.php
@@ -14,7 +14,7 @@ class MyCest
     public function tryToTest(FunctionalTester $I, \Codeception\Example $data)
     {
         $I->amOnPage([$data['url']]);
-        $I->see($data['h1'], 'h1');
+        $I->see($data['h1'], 'li.active>a');
     }
 
     protected function pageProvider()
@@ -23,7 +23,7 @@ class MyCest
             ['url'=>"site/about", 'h1'=>"About"],
             ['url'=>"site/contact", 'h1'=>"Contact"],
             ['url'=>"site/signup", 'h1'=>"Signup"],
-            ['url'=>"site/index", 'h1'=>"Congratulations"],
+            ['url'=>"site/index", 'h1'=>"Home"],
             ['url'=>"site/login", 'h1'=>"Login"],
         ];
     }
